631228a4b7c7ae0f186a9be9c6b711e2516c842b
Vinicius Cunha <ViCunha@outlook.com>
2024-05-08 12:17:25 +0100

    First Commit

---
+++ az204-auth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "1bf98bd7-6869-4801-871e-9ace7a159b53";
+        private const string _tenantId = "f46cdef7-f08a-4654-aabf-f8f77ca67d7b";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
